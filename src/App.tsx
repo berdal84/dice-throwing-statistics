2f0a447fa53687ae8176d460109434e7a075a5b5
Berenger Dalle-Cort <berenger@42borgata.com>
2025-12-14 17:01:51 -0500

    add app menu and scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,41 @@
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import './App.css'
 import Button from '@mui/material/Button';
-import {DeleteForeverOutlined, Undo } from '@mui/icons-material';
+import { DeleteForeverOutlined, Undo } from '@mui/icons-material';
 import Container from '@mui/material/Container';
 import Plot from 'react-plotly.js';
-import { Box } from '@mui/material';
+import { Box, Divider, Grid, Typography } from '@mui/material';
+import AppBarCustom from './AppBarCustom';
 
-const VALUES = [2,3,4,5,6,7,8,9,10,11,12]
-const THEORETICAL_PROBABILITIES = [1,2,3,4,5,6,5,4,3,2,1].map( v => (v / 36) )
+const VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-function App()
-{
+const THEORETICAL_PROBABILITIES = [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1].map(v => (v / 36))
+
+const PAGE = {
+  KEYBOARD: "Keyboard",
+  STATS:  "Statistics",
+  MORE:   "More.."
+}
+
+const PAGES = [
+  PAGE.KEYBOARD,
+  PAGE.STATS,
+  PAGE.MORE
+]
+
+function App() {
   const [history, setHistory] = useState<number[]>([])
 
   const probabilities = useMemo(() => {
     const count = new Array(VALUES.length)
     count.fill(0)
 
-    history.forEach( (value) => count[value-VALUES[0]] += 1 ) // count each number
-    
+    history.forEach((value) => count[value - VALUES[0]] += 1) // count each number
+
     if (history.length > 0) {
-      count.forEach( (_, i) => count[i] /= history.length ) // normalize
+      count.forEach((_, i) => count[i] /= history.length) // normalize
     }
-  
+
     return count
   }, [history])
 
@@ -47,12 +60,12 @@ function App()
     save()
   }, [history])
 
-  const historyRemove = useCallback( (pos: number) => {
-    setHistory( history.toSpliced(pos, 1) )
+  const historyRemove = useCallback((pos: number) => {
+    setHistory(history.toSpliced(pos, 1))
     save()
   }, [history])
 
-  const historyClear = useCallback( () => {
+  const historyClear = useCallback(() => {
     setHistory([])
   }, [history])
 
@@ -61,109 +74,163 @@ function App()
     load()
   }, [])
 
+  const [page, setPage] = useState( PAGES[0] )
+
+  useEffect( () => {
+    const el = document.getElementById(page)
+    if (el)
+    {
+      window.scrollTo({
+        top: el.offsetTop - 100,
+        behavior: 'smooth'
+      });
+    }
+
+  }, [page])
+
   return <>
-    <Container maxWidth="md" sx={{ display: 'flex', flexDirection: 'column', width: '100vw', margin: 0 }}>
-      <h1>Dice Roll Statistics</h1>
-      <h2>Register a new result</h2>
-      <p>Press a number to add a new dice roll result to the history.</p>
-      <Box sx={{ display: 'flex', gap: "2px", justifyContent: 'center', flexWrap: 'wrap' }}>
-        {VALUES.map((value, index) =>
-              <Button
-                key={index}               
-                variant='contained'
-                onClick={() => historyAppend(value)}
+    <Container maxWidth="md">
+      <AppBarCustom
+        pages={PAGES}
+        onPageChange={setPage}
+        title="DR.STATS"
+      />
+      <Box sx={{ display: 'flex', flexDirection: 'column', marginTop: 10, alignItems: 'stretch' }}>
+
+        <Box
+          id={PAGE.KEYBOARD}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100vh',
+          }}>
+
+          <h2>Keyboard</h2>
+
+          <p>Press a number to add a new dice roll result to the history.</p>
+
+          <Grid container
+            rowSpacing={1}
+            gridColumn={2}
+            marginBottom={1}
+          >
+            {VALUES.map((value, index) =>
+              <Grid size={4}>
+                <Button
+                  key={index}
+                  variant='contained'
+                  onClick={() => historyAppend(value)}
                 >
                   {value}
-              </Button>
-           )
-        }
-      </Box>
-      
-      
-      <Box height={500} sx={{ overflow: 'hidden'}}>
-        <h2>Statistics</h2>
-        <Plot
-          style={{ width: '90%', height: '90%'}}
-          data={[{
-            y: VALUES,
-            x: THEORETICAL_PROBABILITIES,
-            text: THEORETICAL_PROBABILITIES.map( x => `${Math.round(x*100)}%`),
-            type: 'bar',
-            marker: { color: '#d2d2d2ff' },
-            name: 'theoretical',
-            visible: "legendonly",
-            orientation: "h",
-          }, {
-            y: VALUES,
-            x: probabilities,
-            text: probabilities.map( x => `${Math.round(x*100)}%`),
-            type: 'bar',
-            marker: { color: '#3283e7ff' },
-            name: 'current game',
-            orientation: "h",
-          }]}
-          layout={{       
-            autosize: true,   
-            legend: {
-              xanchor: "right",
-              bgcolor: "rgba(255,255,255,0.5)",
-            },
-            margin: {
-              pad: 0,
-              //b: 0,
-              t: 0,
-              //l: 20,
-              r: 0
-            },
-            yaxis: {
-              title: {
-                text: "Dice Roll Result"
-              },
-              tickvals: VALUES,
-              ticks: 'outside'
-            },
-            xaxis: {
-              title: {
-                text: "Probabilities"
-              },
-              linewidth: 1,
-              ticks: 'outside'            
-            }}}
-            config={{
-              displayModeBar: false,
-              responsive: true,
-              editable: false,
-              scrollZoom: false,
-              staticPlot: true
-            }}
-        />
-      </Box>
+                </Button>
+              </Grid>
+            )}
+          </Grid>
+          <Divider></Divider>
+          <Box sx={{ display: 'flex', justifyContent: 'stretch', alignItems: 'center' }}>
+            <Typography sx={{ opacity: 0.5, flexGrow: 1 }}>History: ..{history.slice(-10).join(", ")}.</Typography>
 
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <h2>Danger Zone</h2>
-        
-        <p>History of the last 10 dice rolls: ...{history.slice(-10).join(", ") }</p> 
+            <Button
+              variant='text'
+              disabled={history.length === 0}
+              onClick={() => historyRemove(history.length - 1)}
+            >
+              <Undo />UNDO
+            </Button>
 
-        <Button
-          variant='text'
-          disabled={history.length === 0}
-          onClick={() => historyRemove(history.length-1)}
+          </Box>
+
+        </Box>
+
+        <Box
+          id={PAGE.STATS}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100vh'
+          }}>
+            <h2>Statistics</h2>
+            <Plot
+              style={{ width: '90%', height: '100%' }}
+              data={[{
+                y: VALUES,
+                x: THEORETICAL_PROBABILITIES,
+                text: THEORETICAL_PROBABILITIES.map(x => `${Math.round(x * 100)}%`),
+                type: 'bar',
+                marker: { color: '#d2d2d2ff' },
+                name: 'theoretical',
+                //visible: "legendonly",
+                orientation: "h",
+              }, {
+                y: VALUES,
+                x: probabilities,
+                text: probabilities.map(x => `${Math.round(x * 100)}%`),
+                type: 'bar',
+                marker: { color: '#3283e7ff' },
+                name: 'current game',
+                orientation: "h",
+              }]}
+              layout={{
+                autosize: true,
+                legend: {
+                  xanchor: "right",
+                  bgcolor: "rgba(255,255,255,0.5)",
+                },
+                margin: {
+                  pad: 0,
+                  //b: 0,
+                  t: 0,
+                  //l: 20,
+                  r: 0
+                },
+                yaxis: {
+                  title: {
+                    text: "Dice Roll Result"
+                  },
+                  tickvals: VALUES,
+                  ticks: 'outside'
+                },
+                xaxis: {
+                  title: {
+                    text: "Probabilities"
+                  },
+                  linewidth: 1,
+                  ticks: 'outside'
+                }
+              }}
+              config={{
+                displayModeBar: false,
+                responsive: true,
+                editable: false,
+                scrollZoom: false,
+                staticPlot: true
+              }}
+            />
+        </Box>
+
+        <Box
+          id={PAGE.MORE}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100vh'
+          }}>
+          <h2>More..</h2>
+          <p>Be carefull, you can loose your data here.</p>
+          <Button
+            variant='text'
+            color='warning'
+            disabled={history.length === 0}
+            onClick={() => historyClear()}
           >
-          <Undo/> UNDO LAST DICE ROLL
-        </Button>
+            <DeleteForeverOutlined /> CLEAR HISTORY
+          </Button>
+          <p>If you pressed CLEAR HISTORY by mistake, simply refresh the page to restore the previous history. Once you press a number again, you will loose the backup.</p>
+        </Box>
 
-        <Button
-          variant='text'
-          color='warning'
-          disabled={history.length === 0}
-          onClick={() => historyClear()}
-          >
-          <DeleteForeverOutlined/> CLEAR HISTORY
-        </Button>
-        <p>If you pressed CLEAR HISTORY by mistake, simply refresh the page to restore the previous history. Once you press a number again, you will loose the backup.</p>
       </Box>
-      
-  </Container></>
+    </Container>
+    </>
 }
 
 export default App
---
+++ src/AppBarCustom.tsx
@@ -0,0 +1,111 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import { CasinoSharp } from '@mui/icons-material';
+import { useState, type MouseEventHandler} from 'react';
+
+type ResponsiveAppBarProps = {
+  pages: string[],
+  onPageChange: (page: string) => void,
+  title: string
+}
+
+function ResponsiveAppBar({ pages, onPageChange, title = "TITLE" } : ResponsiveAppBarProps )
+{
+  const [open, setOpen] = useState(false)
+
+  const handleOpenNavMenu: MouseEventHandler<HTMLElement> = (_) => {
+    setOpen(true)
+  };
+
+  const handleCloseNavMenu = (page: string | null ) => {
+    if (page != null)
+      onPageChange(page);
+    setOpen(false)
+  };
+
+  return (
+    <AppBar>
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+
+          <CasinoSharp sx={{ display: 'flex', mr: 1 }} />
+          
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: 'flex',
+              fontFamily: 'monospace',
+              fontWeight: 400,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            { title }
+          </Typography>
+        </Box>
+
+        {/* Menu when page is "large" */} 
+        <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={() => handleCloseNavMenu(page) }
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
+        
+        {/* Menu when page is large "small" */} 
+        <Box sx={{ display: { xs: 'flex', sm: 'none' } }}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            open={open}
+            id="menu-appbar"
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            onClose={() => handleCloseNavMenu(null) }
+            sx={{ display: { xs: 'block', md: 'none' } }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page} onClick={() => handleCloseNavMenu(page)}>
+                <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
